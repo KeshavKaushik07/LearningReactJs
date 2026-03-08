005f1cdbfdd1768540219a717d1c96632deffef5
Keshav Kaushik <keshavkaushik955@gmail.com>
2026-03-08 16:03:37 +0530

    use <NavLink> to access isActive

--- src/assets/router/NavBar.jsx
+++ src/assets/router/NavBar.jsx
@@ -8,9 +8,16 @@ const NavBar = () => {
        {/* <a href="/">Home</a>
        <a href="/about">About</a>
        <a href="/contact">Contact</a> Cause page Refresh */}
-       <Link to={"/"}>Home</Link>
+
+
+       {/* <Link to={"/"}>Home</Link>
        <Link to={"/about"}>About</Link>
-       <Link to={"/contact"}>Contact</Link>
+       <Link to={"/contact"}>Contact</Link>  can't access isActive class*/}
+
+
+       <NavLink to={"/"} className={({isActive}) => isActive ? "activeTab":"links"}>Home</NavLink>
+       <NavLink to={"/about"} className={({isActive}) => isActive ? "activeTab":"links"}>About</NavLink>
+       <NavLink to={"/contact"} className={({isActive}) => isActive ? "activeTab":"links"}>Contact</NavLink>  
        </div>
     </>
   )
