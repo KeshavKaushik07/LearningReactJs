3f43a18417d9ac5022ef3d40bd1dd5e1fedd67c3
Keshav Kaushik <keshavkaushik955@gmail.com>
2026-03-08 15:52:02 +0530

    React router method 2

--- src/assets/router/BrowerRouter.jsx
+++ src/assets/router/BrowerRouter.jsx
@@ -1,23 +1,61 @@
+// import React from 'react'
+// import { Home, About, Contact } from "./MultiComponents";
+// import { BrowserRouter, Route, Routes } from 'react-router-dom';
+// import NavBar from './NavBar';
+
+// const BrowerRouter = () => {
+//   return (
+//     <>
+//       <BrowserRouter>
+//         <NavBar />{/*need to show with every component */}
+//         <Routes>
+//           <Route path='/' element={<Home />} />
+//           <Route path='/about' element={<About />} />
+//           <Route path='/contact' element={<Contact />} />
+//         </Routes>
+//       </BrowserRouter>
+//     </>
+//   )
+// }
+
+// export default BrowerRouter
+
+// ________________________________________----------------_________________________________________
+
 import React from 'react'
-import { Home , About , Contact } from "./MultiComponents";
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import { Home, About, Contact } from "./MultiComponents";
 import NavBar from './NavBar';
 
 const BrowerRouter = () => {
+  const router = createBrowserRouter([
+    {
+      path : "/",
+      element : <div>
+        <NavBar/>
+        <Home/>
+      </div>
+    },
+    {
+      path : "/about",
+      element : <div>
+        <NavBar/>
+        <About/>
+      </div>
+    },
+    {
+      path : "/contact",
+      element : <div>
+        <NavBar/>
+        <Contact/>
+      </div>
+    },
+  ])
   return (
     <>
-       <BrowserRouter>
-       <NavBar/>
-       <Routes>
-          <Route path='/' element={<Home/>}/>
-          <Route path='/about' element={<About/>}/>
-          <Route path='/contact' element={<Contact/>}/>
-       </Routes>
-       </BrowserRouter>
+      <RouterProvider router={router} />
     </>
   )
 }
 
 export default BrowerRouter
- 
-// ________________________________________----------------_________________________________________
