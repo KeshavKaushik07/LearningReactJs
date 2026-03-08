7c63205d3c7e773b390cdbdf20015c036de0d147
Keshav Kaushik <keshavkaushik955@gmail.com>
2026-03-08 15:55:00 +0530

    use <Link> to stop page Refreshing

--- src/assets/router/NavBar.jsx
+++ src/assets/router/NavBar.jsx
@@ -1,13 +1,16 @@
 import React from 'react'
-import { NavLink } from 'react-router-dom'
+import { Link, NavLink } from 'react-router-dom'
 
 const NavBar = () => {
   return (
     <>
     <div className="navRoutes">
-       <a href="/">Home</a>
+       {/* <a href="/">Home</a>
        <a href="/about">About</a>
-       <a href="/contact">Contact</a>
+       <a href="/contact">Contact</a> Cause page Refresh */}
+       <Link to={"/"}>Home</Link>
+       <Link to={"/about"}>About</Link>
+       <Link to={"/contact"}>Contact</Link>
        </div>
     </>
   )
