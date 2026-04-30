7d9affc5669ac00759e348780a7b0062f9102535
Keshav Kaushik <keshavkaushik955@gmail.com>
2026-04-30 12:41:36 +0530

    learn Builder in RTK

--- src/assets/Redux_Tool_Kit/ReduxComponents/Profile.jsx
+++ src/assets/Redux_Tool_Kit/ReduxComponents/Profile.jsx
@@ -4,13 +4,17 @@ import { useDispatch, useSelector } from 'react-redux'
 const Profile = () => {
     const name = useSelector((state) => state.name);
     const age = useSelector((state) => state.age);
+    const status = useSelector((state) => state.status);
     const dispatch = useDispatch();
     return (
         <>
             <h1>Profile ...{` name : ${name} `}</h1>
             <h1>age : {age}</h1>
+            <h1>status : {status}</h1>
 
-            <button onClick={() => dispatch({ type: "update_age", payload: 40 })}>update Age</button>
+            <button onClick={() => dispatch({ type: "update_age", payload: 40 })}>update Age</button><br/>
+            <button onClick={() => dispatch({ type: "update_name", payload: "hello" })}>update name</button><br/>
+            <button onClick={() => dispatch({ type: "update_status", payload: "Doctor" })}>update status</button><br/>
         </>
     )
 }
--- src/assets/Redux_Tool_Kit/userStore.js
+++ src/assets/Redux_Tool_Kit/userStore.js
@@ -1,12 +1,26 @@
+import { createReducer } from "@reduxjs/toolkit"
+
 const initialState = {
     name : "mohit",
     age : 20,
     status : "developer"
   }
 
-export default (state = initialState,action) => {
-    if(action.type == "update_age")
-        return { ...state,age : action.payload }
-    return state;
-     
-    }
+// export default (state = initialState,action) => {
+//     if(action.type == "update_age")
+//         return { ...state,age : action.payload }
+//     return state;
+//     }
+
+export default createReducer(initialState,(builder)=>{
+    builder.addCase("update_age",(state,action)=>{
+        state.age = action.payload
+    });
+    builder.addCase("update_name",(state,action)=>{
+        state.name = action.payload
+    });
+    builder.addCase("update_status",(state,action)=>{
+        state.status = action.payload
+    });
+
+});
